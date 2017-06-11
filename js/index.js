87e8bf5722f3dcff282c2c43b8628044378dbafc
bliugriz <adelce.g@gmail.com>
2017-06-11 12:40:00 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){  
+$("#pic1").click(function(){
+    $("#pic1").hide();
+	});
+	
+	$("#pic2").click(function(){
+    $("#pic2").hide();
+	});
+	
+	$("#pic3").click(function(){
+    $("#pic3").hide();
+	});
+	
+	
+	$("#dood1").click(function(){
+    $("#dood1").hide();
+	});
+	$("#dood2").click(function(){
+    $("#dood2").hide();
+	});
+	$("#dood3").click(function(){
+    $("#dood3").hide();
+	});
+	$("#dood4").click(function(){
+    $("#dood4").hide();
+	});
+	
+	
+	$("#vid1").click(function(){
+    $("#vid1").hide();
+	});
+	
+}); 
