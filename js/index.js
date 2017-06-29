aa44400466cf5595fc473579f49df89c1a770f7c
bliugriz <adelce.g@gmail.com>
2017-06-30 00:36:36 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,198 @@
+$(document).ready(function(){
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 3000);
+	
+	$("#pic1").click(function(){
+    $("#pic1").hide();
+	}); 
+	$("#pic2").click(function(){
+    $("#pic2").hide();
+	}); 
+	$("#pic3").click(function(){
+    $("#pic3").hide();
+	}); 
+	$("#pic4").click(function(){
+    $("#pic4").hide();
+	}); 
+	$("#pic5").click(function(){
+    $("#pic5").hide();
+	}); 
+	$("#pic7").click(function(){
+    $("#pic7").hide();
+	});
+	$("#pic8").click(function(){
+    $("#pic8").hide();
+	});
+	$("#pic9").click(function(){
+    $("#pic9").hide();
+	});
+	$("#pic10").click(function(){
+    $("#pic10").hide();
+	});
+	$("#pic11").click(function(){
+    $("#pic11").hide();
+	});
+	
+	
+	$("#pic12").click(function(){
+    $("#pic12").hide();
+	});
+	
+	$("#pic13").click(function(){
+    $("#pic13").hide();
+	$('#vid2').get(0).play()
+	});
+	
+	$("#pic12a").click(function(){
+    $("#pic12a").hide();
+	});
+	
+	$("#pic13a").click(function(){
+    $("#pic13a").hide();
+	});
+	
+	
+	
+	$("#pic14").click(function(){
+    $("#pic14").hide();
+	});
+	$("#pic15").click(function(){
+    $("#pic15").hide();
+	});
+	$("#pic16").click(function(){
+    $("#pic16").hide();
+	});
+	$("#pic17").click(function(){
+    $("#pic17").hide();
+	});
+	$("#pic18").click(function(){
+    $("#pic18").hide();
+	});
+	$("#pic19").click(function(){
+    $("#pic19").hide();
+	});
+	$("#pic20").click(function(){
+    $("#pic20").hide();
+	});
+	$("#pic21").click(function(){
+    $("#pic21").hide();
+	});
+	$("#pic22").click(function(){
+    $("#pic22").hide();
+	});
+	$("#pic23").click(function(){
+    $("#pic23").hide();
+	});
+	$("#pic24").click(function(){
+    $("#pic24").hide();
+	});
+	$("#pic25").click(function(){
+    $("#pic25").hide();
+	});
+	$("#pic26").click(function(){
+    $("#pic26").hide();
+	});
+	$("#pic27").click(function(){
+    $("#pic27").hide();
+	});
+	$("#pic28").click(function(){
+    $("#pic28").hide();
+	});
+	$("#pic29").click(function(){
+    $("#pic29").hide();
+	});
+	$("#pic30").click(function(){
+    $("#pic30").hide();
+	});
+	$("#pic31").click(function(){
+    $("#pic31").hide();
+	});
+	$("#pic32").click(function(){
+    $("#pic32").hide();
+	});
+	$("#pic33").click(function(){
+    $("#pic33").hide();
+	});
+	$("#pic34").click(function(){
+    $("#pic34").hide();
+	});
+	$("#pic35").click(function(){
+    $("#pic35").hide();
+	});
+	$("#pic36").click(function(){
+    $("#pic36").hide();
+	});
+	$("#pic37").click(function(){
+    $("#pic37").hide();
+	});
+	$("#pic38").click(function(){
+    $("#pic38").hide();
+	});
+	$("#pic39").click(function(){
+    $("#pic39").hide();
+	});
+	$("#pic40").click(function(){
+    $("#pic40").hide();
+	});
+	
+	
+	
+	
+	
+	
+	$("#dood1").click(function(){
+    $("#dood1").hide();
+	});
+	$("#dood2").click(function(){
+    $("#dood2").hide();
+	$('#vid1').get(0).play()
+	});
+	$("#dood3").click(function(){
+    $("#dood3").hide();
+	});
+	$("#dood4").click(function(){
+    $("#dood4").hide();
+	});
+	$("#dood5").click(function(){
+    $("#dood5").hide();
+	});
+	$("#dood6").click(function(){
+    $("#dood6").hide();
+	});
+	$("#dood7").click(function(){
+    $("#dood7").hide();
+	});
+	$("#dood8").click(function(){
+    $("#dood8").hide();
+	});
+	$("#dood9").click(function(){
+    $("#dood9").hide();
+	});
+	$("#dood10").click(function(){
+    $("#dood10").hide();
+	});
+	$("#dood11").click(function(){
+    $("#dood11").hide();
+	});
+	$("#dood12").click(function(){
+    $("#dood12").hide();
+	});
+	$("#dood13").click(function(){
+    $("#dood13").hide();
+	});
+	
+	
+	
+	$("#vid1").click(function(){
+    $("#vid1").hide();
+	$('#vid1').get(0).pause()
+	});
+	$("#vid2").click(function(){$.data(this, 'timer', setTimeout(function() {
+    $("#vid2").hide();
+	$('#vid2').get(0).pause();
+	 }, 1000));
+	});
+	
+});
